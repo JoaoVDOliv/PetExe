260a7d9c4c8e59f26e16dbe5e834e3f10df8715e
João Vítor Dorta <olivdortajoao99@gmail.com>
2024-06-25 22:01:44 -0300

    first commit

---
+++ PetExe/Menu.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetExe
+{
+    public class Menu
+    {
+        public void startMenu() 
+        {
+            int menuIterator = 0;
+
+            do
+            {
+                Console.WriteLine("Escolha uma opção para podermos começar: ");
+                Console.WriteLine("1-Cadastro de Pet");
+                Console.WriteLine("0-Sair");
+                
+                menuIterator = Convert.ToInt32(Console.ReadLine());
+
+                switch (menuIterator) 
+                {
+                    case 0:
+                        Console.WriteLine("Até mais");
+                        break;
+                    case 1:
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida");
+                        Console.WriteLine("Digite novamente");
+                        break;
+                }
+
+            } while (menuIterator != 0);
+        }
+    }
+}
---
+++ PetExe/Program.cs
@@ -0,0 +1,17 @@
+﻿using PetExe;
+
+Console.WriteLine("_______________________________________");
+Console.WriteLine("Bem vindo ao central de ajuda pets");
+Console.WriteLine("Este aplicativo tem a função de ajudar no ");
+Console.WriteLine("cadastro e gerenciamento de pets perdidos durante");
+Console.WriteLine("a catástrofe do Rio Grande do Sul");
+Console.WriteLine("_______________________________________");
+
+Menu menu = new Menu();
+menu.startMenu();
+
+
+
+
+
+
